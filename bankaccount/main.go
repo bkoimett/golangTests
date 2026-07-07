198b27187b83e93f1f8b94b5f4c82f7793250c77
bkoimett <benkoimet@gmail.com>
2026-07-07 12:22:55 +0300

    Implement Wallet struct and AddMoney method in main.go

---
+++ bankaccount/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Wallet struct {
+	Owner string
+	Balance float64
+}
+
+func (w *Wallet) AddMoney(amount float64) {
+	w.Balance += amount
+}
+
+func main() {
+	myWallet := Wallet{"Benjamin",50.0}
+
+	myWallet.AddMoney(25.0)
+
+	fmt.Println(myWallet.Balance)
+}
